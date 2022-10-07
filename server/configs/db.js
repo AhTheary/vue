cb35e8629a4ced44c014e86ef755c6139a036e2b
KHEANG Sokuntheary <86469224+AhTheary@users.noreply.github.com>
2022-10-07 17:14:20 +0700

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports = async() => {
     try {
-        await mongoose.connect('mongodb://52.91.219.193:27017/mgdb_tp12_2');
+        await mongoose.connect('mongodb://localhost:27017/mgdb_tp12_2');
         console.log("MongoDB connected :(");
     } catch (error) {
         console.log("MongoDB err: ", error);
